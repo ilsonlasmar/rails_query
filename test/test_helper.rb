d4cb59627ab95958441d6518f61cae4743d0509d
Ilson Lasmar <ilson@MacBook-Air-de-Ilson.local>
2026-05-02 12:23:31 -0300

    test: setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "rails_query"
+require "simplecov"
+SimpleCov.start do
+  add_filter "/test/"
+end
 
 require "minitest/autorun"
+require "mocha/minitest"
+
+require "rails_query"
+
+# cache store configuration for testing
+RailsQuery.configure do |config|
+  config.cache_store = ActiveSupport::Cache::MemoryStore.new
+end
